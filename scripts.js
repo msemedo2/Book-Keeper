21673b69392b13377ec2ad188e9b1760ea817e0f
unknown <miguel.semedo.2@outlook.com>
2022-10-01 12:45:54 +0100

    Implement Delete Bookmarks

--- scripts.js
+++ scripts.js
@@ -45,6 +45,8 @@ function validate(nameValue, urlValue) {
 
 // Build Bookmarks DOM
 function buildBookmarks() {
+	// Remove all bookmark elements
+	bookmarksContainer.textContent = '';
 	// Build Items
 	bookmarks.forEach((bookmark) => {
 		const { name, url } = bookmark;
@@ -94,6 +96,19 @@ function fetchBookmarks() {
 		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 	}
 	buildBookmarks();
+}
+
+// Delete Bookmark
+function deleteBookmark(url) {
+	bookmarks.forEach((bookmark, i) => {
+		if (bookmark.url === url) {
+			bookmarks.splice(i, 1);
+			console.log(bookmarks);
+		}
+	});
+	// Update bookmarks array in localStorage, re-populate DOM
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	fetchBookmarks();
 }
 
 // Handle Data from Form
